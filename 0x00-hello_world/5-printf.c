cf5676acf9e2157c404a3ce44cabb712addc25b9
teemah04 <fateemahtoyin4@gmail.com>
2022-10-13 11:14:42 -0700

    C programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main:serves as the entry point of the program.
+ * return:by default returns zero if there are no errors otherwise prints a non-zero value.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
