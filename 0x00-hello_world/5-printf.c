9034b572eef122828103566193a5bd0f3df64490
teemah04 <fateemahtoyin4@gmail.com>
2022-10-13 11:29:52 -0700

    C programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main:serves as the entry point of the program.
- * return:by default returns zero if there are no errors otherwise prints a non-zero value.
+ * main-entry point of the program.
+ *
+ * Return:by default returns zero if there are no errors otherwise prints a non-zero value.
  */
 int main(void)
 {
