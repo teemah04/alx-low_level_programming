7232cfded6483c91081628395db747c9f2eca6f5
teemah04 <fateemahtoyin4@gmail.com>
2022-10-13 10:44:34 -0700

    C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main:it serves as the entry point of the program
+ * Return:By default returns zero if there is no errot otherwise returns non-zero value.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
+}
