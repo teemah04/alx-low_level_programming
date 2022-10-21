d08ac473b6c5c05930753b1d09a4aa4c040f00db
teemah04 <fateemahtoyin4@gmail.com>
2022-10-21 04:17:25 -0700

    C programming 4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,7 +5,6 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int x, int y);
 void print_numbers(void);
-int _putchar(char);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
