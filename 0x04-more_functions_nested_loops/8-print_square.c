6cc8ab11f5555900afe6616cadb51f7e52aa77f9
teemah04 <fateemahtoyin4@gmail.com>
2022-10-21 09:25:27 -0700

    C programming 4

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,7 +2,8 @@
 
 /**
  * print_square-prints hashes squares
- * @size: size of the square.
+ * @size:size of the square.
+ *
  * Return:no return
  */
 void print_square(int size)
@@ -16,7 +17,7 @@ void print_square(int size)
 			_putchar(35);
 		}
 		if (i != size - 1)
-			_puthar('\n');
+			_putchar('\n');
 	}
 	_putchar('\n');
 }
