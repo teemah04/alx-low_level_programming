337d1215a9e9dc609df976addf28a9ccac3565ad
teemah04 <fateemahtoyin4@gmail.com>
2022-11-06 12:30:39 -0800

    solutions to argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main-Entry point
+ * @argc:argument count
+ * @argv: argument vector
+ * Return:0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("5s\n", argv[0]);
+
+	exit(EXIT_SUCCES);
+}
