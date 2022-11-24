07a632870745ef77ce382de5ff9609bc508dfe54
teemah04 <fateemahtoyin4@gmail.com>
2022-11-24 11:39:50 -0800

    singlylinks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,9 +8,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t noelm;
+	size_t nelem;
 
-	noelm = 0;
+	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		noelm++;
+		nelem++;
 	}
-	return (noelm);
+	return (nelem);
 }
